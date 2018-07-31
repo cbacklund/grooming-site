f13c6e5bff6888ab23bb40e3d24d15698ed20ffc
unknown <Joseph.Kampa@gmail.com>
2018-07-30 20:47:28 -0500

    Added Routes, Folder Restructure

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import AboutUs from "./components/Pages/AboutUS";
+import ContactUs from "./components/Pages/ContactUs";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={AboutUs} />
+                <Route exact path="/contact" component={ContactUs} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Pages/ContactUs/ContactUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./ContactUs.css";
+
+const ContactUs => {
+
+
+}; 
+
+export default ContactUs;
