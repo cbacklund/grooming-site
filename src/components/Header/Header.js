a3ab6d38a05a385d62db1a2d19ddd76ad269a40d
Al Bassett <albassett@er-10-129-128-191.wireless.umn.edu>
2018-07-30 19:19:55 -0500

    adding a basic header & staff.js card

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = props => (
+    <div className="jumbotron jumbotronn-fluid">
+        <h1 className="display-4 text-left">Groomers Anonymous</h1>
+
+    
+    </div>     
+);
+
+export default Header;
