cd91cfd7ebc6c6d9bb2645870d13fcc7404c9b38
Amanda Finstad <amanda.finstad@prescott.edu>
2018-08-01 19:43:17 -0500

    Components

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./Header.css";
+
 const Header = props => (
     <div className="jumbotron jumbotronn-fluid">
         <h1 className="display-4 text-left">Groomers Anonymous</h1>
