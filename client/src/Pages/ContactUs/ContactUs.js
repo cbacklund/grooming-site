6dcfe501d403661b0bf57c8fdf653c1057d4fa24
Amanda Finstad <amanda.finstad@prescott.edu>
2018-08-04 11:00:08 -0500

    Contact Us
    deleted undefined styles

--- client/src/Pages/ContactUs/ContactUs.js
+++ client/src/Pages/ContactUs/ContactUs.js
@@ -6,8 +6,8 @@ import "./ContactUs.css";
 // We use JSX curly braces to evaluate the style object
 
 const Contact = () => (
-  <header style={styles.contactStyle} className="contact">
-    <h1 style={styles.contactStyle}>Contact Us</h1>
+  <header className="contact">
+    <h1>Contact Us</h1>
   </header>
 );
 
