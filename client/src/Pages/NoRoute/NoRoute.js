a80c8e6c98e9c9b57e65d6c749275bc544c63f31
unknown <Joseph.Kampa@gmail.com>
2018-08-23 21:01:02 -0500

    Updated the 404 page

--- client/src/Pages/NoRoute/NoRoute.js
+++ client/src/Pages/NoRoute/NoRoute.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
-import Error404 from "../../components/img/404.jpg";
+import Carousel from "nuka-carousel";
+import Dog1 from "../../components/img/dogs/harrier-pricing.png"
+import Dog2 from "../../components/img/dogs/chihuahuah-short-hair-pricing.png"
+import Dog3 from "../../components/img/dogs/affenpinscher-pricing.png"
+import Dog4 from "../../components/img/dogs/siberian-husky-pricing-pic.png"
+import Dog5 from "../../components/img/dogs/scottish-deerhound-pricing.png"
 import "./NoRoute.css";
 
 class NoRoute extends Component {
@@ -12,13 +17,26 @@ class NoRoute extends Component {
     render() {
         return (
             <Container fluid>
-            <br />
-            <br />
+            <Row>
+                <Col size="md-2">
+                </Col>
+                <Col size="md-7">
+                <h1>
+                    Page Not Found, so look at some dogs!
+                </h1>
+                </Col>
+            </Row>
                 <Row>
                     <Col size="md-2">
                     </Col>
                     <Col size="md-8">
-                        <img id="image404" src={Error404} alt="Error 404" class="center" />
+                        <Carousel>
+                            <img src={Dog1} alt="Dog1" class="image404" />
+                            <img src={Dog2} alt="Dog2" class="image404" />
+                            <img src={Dog3} alt="Dog3" class="image404" />
+                            <img src={Dog4} alt="Dog4" class="image404" />
+                            <img src={Dog5} alt="Dog5" class="image404" />
+                        </Carousel>
                     </Col>
                 </Row>
             </Container>
